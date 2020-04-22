bbe73d11f73770f2759e0aa7717c5b2f9b52de0d
hzz <dexter.ho.cn@gmail.com>
2020-04-23 01:03:32 +0800

    feat: add Decorator

---
+++ Decorator/DecoratorLogo.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Description:
+ * Author: DexterHo <dexter.ho.cn@gmail.com>
+ * Date: 2020/4/23
+ * Time: 0:44
+ * Created by PhpStorm.
+ */
+
+namespace Decorator;
+
+class DecoratorLogo
+{
+    protected $TShirtApi;
+
+    public function __construct(TShirtApi $TShirtApi)
+    {
+        $this->TShirtApi = $TShirtApi;
+    }
+
+    public function product()
+    {
+        // TODO: Implement product() method.
+        $this->TShirtApi->product();
+        return $this;
+    }
+
+    public function decorate($value)
+    {
+        // TODO: Implement decorate() method.
+
+        return "add brand $value" . PHP_EOL;
+    }
+}
